e32aed97effc12c80e1b00d9459c309d8f39a0f8
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-01-21 00:52:51 +0900

    feat: Reactのイベントやstyleについて

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    alert("押されました");
+  };
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "17px"
+  };
   // 複数行のjsxをreturnする場合は()で囲む
   // jsxは一つのタグでreturnしなければならないので
   // 以前はdivで囲っていたが、今は<></>で囲むようにしている
+
+  // styleの外側の{}はjsを書きますという宣言用、内側の{}はオブジェクトを表す
+  // また 上のcontentStyleのように オブジェクト形式のプロパティを用意しておいてstyleの中で読み込ませることが可能
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
