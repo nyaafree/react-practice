e01703b0ea3811c3c6c56946d8331d09d426f46b
nyaafree <45682975+nyaafree@users.noreply.github.com>
2022-01-21 00:28:51 +0900

    feat: jsxの基本的な記法学習

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+// jsxを仕様するために下記の記述が必要だったが、React Ver17以降は書かなくても良い
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  // 複数行のjsxをreturnする場合は()で囲む
+  // jsxは一つのタグでreturnしなければならないので
+  // 以前はdivで囲っていたが、今は<></>で囲むようにしている
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDom.render(<App />, document.getElementById("root"));
